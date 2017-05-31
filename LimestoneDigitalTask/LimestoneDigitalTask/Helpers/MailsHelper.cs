33deb42f08e554190c04f158a994b8e097963eeb
Roman Bezverhniy <bezvra@gmail.com>
2017-05-31 19:20:29 +0300

    - Добавил и настроил механизм для работы с почтой.

---
+++ LimestoneDigitalTask/LimestoneDigitalTask/Helpers/MailsHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using LimestoneDigitalTask.Models.DTO;
+
+namespace LimestoneDigitalTask.Helpers
+{
+    public class MailsHelper
+    {
+        private SmtpClient smtpClient { get; set; }
+        private MailMessage message { get; set; }
+
+        public MailsHelper()
+        {
+            //var companyMail = "mailname@yandex.ua";
+            //var password = "pas$w0rd#";
+
+            smtpClient = new SmtpClient();
+            smtpClient.Host = "smtp.yandex.ua";
+            smtpClient.Port = 25;
+            smtpClient.EnableSsl = true;
+            smtpClient.Timeout = 2 * 60 * 1000;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtpClient.Credentials = new NetworkCredential(companyMail, password);
+
+            message = new MailMessage();
+            message.From = new MailAddress(companyMail);
+            message.IsBodyHtml = true;
+        }
+
+        public void Send(string email, List<MailCartDTO> list, string promoCode)
+        {
+            message.To.Add(email);
+            message.Subject = "Store: shopping cart";
+
+            var cart = "";
+            var sum = 0m;
+            foreach (var item in list)
+            {
+                cart += item.Name + ": " + item.Price + "<br/>";
+                sum += item.Price;
+            }
+
+            message.Body = "Hello!<br/><br/>Your shopping cart:<br/>" + cart + "<hr/>Total:" + sum;
+            message.Body += string.IsNullOrEmpty(promoCode) ? "<br/><br/>--<br/>Best regards, Store team." : "Used promocode: " + promoCode + "<br/><br/>--<br/>Best regards, Store team.";
+
+            smtpClient.Send(message);
+        }
+    }
+}
---
+++ LimestoneDigitalTask/LimestoneDigitalTask/Models/DTO/ShoppingCartDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LimestoneDigitalTask.Models.DTO
+{
+    public class MailCartDTO
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
